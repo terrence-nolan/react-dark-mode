c041ffa88d0a58f3a5830d3ae27a6085278f991d
terrence-nolan <terrencenolan@protonmail.com>
2024-03-23 22:59:45 -0400

    add colors

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 export const App = () => {
   return (
-    <div className="App">
+    <div className="App" data-theme="dark">
       <h1 className="title">Hello world!</h1>
       <div className="box">
         <h2>This is a box</h2>
