b53a712e6e0f442e78d710875dd27abd6a510caa
terrence-nolan <terrencenolan@protonmail.com>
2024-03-23 22:46:20 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./App.css";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <h1 className="title">Hello world!</h1>
+      <div className="box">
+        <h2>This is a box</h2>
+      </div>
+    </div>
+  )
+}
